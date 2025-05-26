bd6aa06d2636ee020fa0b947b9fa76a7b3a4a03f
Black <islepuchin@yandex.ru>
2025-05-26 14:20:53 +0300

    Подключение БД, актуализация версии Spring, исправление ДТО

--- src/main/java/ru/skypro/homework/dto/NewPassword.java
+++ src/main/java/ru/skypro/homework/dto/NewPassword.java
@@ -2,7 +2,7 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Size;
 
 // Обновление пароля пользователя
 @Data
